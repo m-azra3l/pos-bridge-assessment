37110369058fc824029e4624ab62fa38f3b10bee
Michael <gadreelazazel@gmail.com>
2023-06-15 18:52:23 +0100

    create

---
+++ metadata/contractAddress.js
@@ -0,0 +1,4 @@
+
+    export const nftAddress = "0xD8F6847fD533A93eeCD470de80685665bDca8c06"
+    const childAddress = "0xD59b56cE9afd05B8226372DA93F4B488FBf3Ac34"
+  
---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: 'Uhanmi V1',
+        description: 'This is the first or cover image of Uhanmi NFT collection.',
+        image: 'QmXxtXrGznUsGoyMCRqCKVU53cuJPeQFxJHmWWiZ74TLvv'
+    },
+    {
+        name: 'Uhanmi V2',
+        description: 'This is a generic image NFT in the Uhanmi NFT collection.',
+        image: 'QmQABtbWHUHsLC381jnuLABQGUb5qgMMoKbgHkM59cqkGX'
+    },
+    {
+        name: 'Uhanmi V3',
+        description: 'This is a generic image NFT in the Uhanmi NFT collection.',
+        image: 'QmeYRcmmLELdnAJM7CzoZfW6CLBD8PoZniHyP96GKkcYqx'
+    },
+    {
+        name: 'Uhanmi V4',
+        description: 'This is a generic image NFT in the Uhanmi NFT collection.',
+        image: 'QmR56wLp3AxqfNLyfJxu16qXcNyG3dyGn1kXQscTkJ6ntK'
+    },
+    {
+        name: 'Uhanmi V5',
+        description: 'This is the ultimate image NFT in the Uhanmi NFT collection.',
+        image: 'QmeDS4WBasN4UaTJMZTJjEEwyxs4wL8GT7GoYWrXrN5Dqr'
+    },    
+];
+
+module.exports = nfts;
